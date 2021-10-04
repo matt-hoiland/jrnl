eab553e2ac3541a06fe449ee13717beacd6361a8
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-10-04 11:12:56 -0600

    Move source code back to top level

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "github.com/matt-hoiland/jrnl/app/jrnl/cmd"
+import "github.com/matt-hoiland/jrnl/cmd"
 
 func main() {
 	cmd.Execute()
